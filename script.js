2aa28a9e2c038593d6dcb5d253710a158b35a541
surk19 <shafiqurk@gmail.com>
2026-06-11 18:34:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
-if (menuButton && nav) {
-  menuButton.addEventListener('click', () => nav.classList.toggle('open'));
-}
